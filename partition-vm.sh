1375666a051b3e882e74fef1fca390472787bbc5
andreaugustoaragao <andrearag@gmail.com>
2023-11-13 16:41:32 -0700

    automate vm installation

--- partition-vm.sh
+++ partition-vm.sh
@@ -7,6 +7,25 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "Please enter the destination storage device (sda,vda,nvme01): "
+read user_input
+
+# Echo the input back
+echo "NixOS will be installed to ./dev/$user_input. Continue (y/n): "
+
+# Ask for confirmation
+while true; do
+    read -p "Do you wish to proceed? (yes/no) " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Exiting."; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+# Continue with the rest of the script
+echo "Creating partitions."
+# ... rest of your script
 # Rest of your script...
 echo creating partitions...
 parted /dev/sda -- mklabel gpt
@@ -15,7 +34,7 @@ parted /dev/sda -- mkpart primary linux-swap -8GB 100%
 parted /dev/sda -- mkpart ESP fat32 1MB 512MB
 parted /dev/sda -- set 3 esp on
 
-echo formatting...
+echo "Formatting partitions."
 mkfs.ext4 -L nixos /dev/sda1
 mkswap -L swap /dev/sda2
 mkfs.fat -F 32 -n boot /dev/sda3
@@ -24,7 +43,7 @@ mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
 swapon /dev/sda2
 
-echo generating config...
+echo "Generating config."
 nixos-generate-config --root /mnt
 
 
