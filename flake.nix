b771a6b103e4af5fed494ef0ee1d3c9b149a2aba
andreaugustoaragao <andrearag@gmail.com>
2023-11-13 09:04:34 -0700

    creating utm vm config

--- flake.nix
+++ flake.nix
@@ -30,6 +30,7 @@
             dpi = 192;
           };
         in {
+#TODO: AVOID THE REPETITION  
           prl-dev = nixpkgs.lib.nixosSystem rec{
             system = "aarch_64-linux";
             specialArgs = {
@@ -71,6 +72,53 @@
                 };
               }
               ./machines/prl-dev
+              ./system
+              ./home
+              nix-index-database.nixosModules.nix-index #https://github.com/nix-community/nix-index-database
+            ];
+          };
+
+          utm-dev = nixpkgs.lib.nixosSystem rec{
+            system = "aarch_64-linux";
+            specialArgs = {
+              inherit (nixpkgs) lib;
+              inherit inputs nixpkgs;
+              inherit system;
+              inherit userDetails;
+              inherit desktopDetails;
+            };
+
+            modules = [
+              inputs.home-manager.nixosModules.home-manager
+              {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  users.${userDetails.userName} = {
+                    home = {
+                      username = "${userDetails.userName}";
+                      homeDirectory = "/home/${userDetails.userName}";
+                      # do not change this value
+                      stateVersion = "23.11";
+                    };
+
+                    # Let Home Manager install and manage itself.
+                    programs.home-manager.enable = true;
+                  };
+                  # make root great again
+ 							    users.root = { pkgs, ...}:{
+								    home.username = "root";
+								    home.homeDirectory = "/root";
+								    home.stateVersion = "23.11";
+								    programs.home-manager.enable = true;
+								    imports = [
+									    ./home/nvim.nix
+									    ./home/shell.nix
+								    ];
+								  };
+                };
+              }
+              ./machines/utm-dev
               ./system
               ./home
               nix-index-database.nixosModules.nix-index #https://github.com/nix-community/nix-index-database
--- machines/utm-dev/hardware-configuration.nix
+++ machines/utm-dev/hardware-configuration.nix
@@ -6,7 +6,7 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "usbhid" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "virtio_pci" "xhci_pci" "usbhid" "usb_storage"];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
@@ -21,7 +21,8 @@
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ 
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
