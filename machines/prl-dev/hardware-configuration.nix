d2ecc6340f4454657cfd59800d3ff7a3cae2a1a4
andreaugustoaragao <andrearag@gmail.com>
2023-11-13 11:57:50 -0700

    using labels to identify disks

--- machines/prl-dev/hardware-configuration.nix
+++ machines/prl-dev/hardware-configuration.nix
@@ -12,23 +12,29 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/178f1cf0-ec3e-459d-9be4-52500527ee38";
+    { device = "/dev/disk/by-label/nixos";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/499E-70F7";
+    { device = "/dev/disk/by-label/boot";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices = [ 
+    {
+      device = "/dev/disk/by-label/swap";
+    }
+  ];
+
+
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  # networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp0s5.useDHCP = lib.mkDefault true;
+  networking.useDHCP = lib.mkDefault true;
+  #networking.interfaces.enp0s5.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
   hardware.parallels.enable = true;
