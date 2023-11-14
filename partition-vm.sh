9c38f4101b873fd73ee8c880f40ca02ee14bcb60
andreaugustoaragao <andrearag@gmail.com>
2023-11-13 17:52:43 -0700

    automate vm installation

--- partition-vm.sh
+++ partition-vm.sh
@@ -18,25 +18,23 @@ if [ "$num_devices" -gt 1 ]; then
     read -p "Please enter the destination storage device: " device < /dev/tty
 fi
 
-echo "NixOS will be installed to /dev/$user_input. Continue (y/n): "
-
 while true; do
-    read -p "Do you wish to proceed? (yes/no) " yn < /dev/tty
+    read -p "NixOS will be installed to /dev/$device. Proceed (y/n)? " yn < /dev/tty
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) echo "Exiting."; exit;;
-        * ) echo "Please answer yes or no.";;
+        * ) echo "Please answer y or n.";;
     esac
 done
 
-echo "Creating partitions."
+echo "Creating partitions"
 parted /dev/sda -- mklabel gpt
 parted /dev/sda -- mkpart primary 512MB -8GB
 parted /dev/sda -- mkpart primary linux-swap -8GB 100%
 parted /dev/sda -- mkpart ESP fat32 1MB 512MB
 parted /dev/sda -- set 3 esp on
 
-echo "Formatting partitions."
+echo "Formatting partitions"
 mkfs.ext4 -L nixos /dev/sda1
 mkswap -L swap /dev/sda2
 mkfs.fat -F 32 -n boot /dev/sda3
@@ -45,7 +43,11 @@ mkdir -p /mnt/boot
 mount /dev/disk/by-label/boot /mnt/boot
 swapon /dev/sda2
 
-echo "Generating config."
-nixos-generate-config --root /mnt
+#echo "Generating config."
+#nixos-generate-config --root /mnt
 
+nix-env -iA nixos.git
+nix-env -iA nixos.nixFlakes
+
+nixos-install --flake https://github.com/andreaugustoaragao/nix-config#utm-dev
 
