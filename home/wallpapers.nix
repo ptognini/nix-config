d79be9bfbd7247c8625c2b0db073a67691e26b1a
Pier Tognini <ptognini@avaya.com>
2024-02-14 12:27:13 -0700

    fix typo

--- home/wallpapers.nix
+++ home/wallpapers.nix
@@ -5,7 +5,7 @@ pkgs.stdenv.mkDerivation {
   src = pkgs.fetchgit {
     url = "https://github.com/rose-pine/wallpapers";
     #rev = "main";  # Specify the commit, tag, or branch you want to download.
-    sha256 = "1qp/8ACQFvw8nOiO1N08o7s+zsQeZPWopZvG/Ox50ws=";
+    sha256 = "1qp/8ACQFvm8nOiO1N08o7s+zsQeZPWopZvG/Ox50ws=";
   };
 
   installPhase = ''
